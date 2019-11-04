01b434c94c3669d74c9742031a2592be0ed45b90
IFlickerz <nicolasmossmannlemos@Gmail.com>
2019-11-04 16:34:49 -0300

    Adding the products page

--- Banco de Dados/KabumTega.sql
+++ Banco de Dados/KabumTega.sql
@@ -28,9 +28,12 @@ ON DELETE CASCADE
 
 CREATE TABLE IF NOT EXISTS Produto (
 Id_produto BIGINT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+Titulo VARCHAR(255) NOT NULL,
+Imagem VARCHAR(255) NOT NULL,
 Quantidade INT,
 Descricao VARCHAR(255) NOT NULL,
-Preço DECIMAL(10,2) NOT NULL
+Categoria VARCHAR(255) NOT NULL,
+Preco DECIMAL(10,2) NOT NULL
 );
 
 
@@ -59,6 +62,24 @@ Id_produto BIGINT,
 DATA_da_Compra DATE,
 FOREIGN KEY(Id_cliente) REFERENCES Cliente (Id_cliente),
 FOREIGN KEY(Id_produto) REFERENCES Produto (Id_produto)
-)
+);
 
-
+INSERT INTO Produto
+VALUES (null,'NVIDIA GTX 1080 TI','1080ti.jpg', 12, 'A mais poderosa placa de vídeo da série GTX 1000 da NVIDIA.', 'Placadevideo', '2299.90'),
+(null, 'NVIDIA RTX 2080 TI','2080ti.png', 5, 'Essa é a placa de vídeo mais avançada já feita, possuí um poder de processamento de outro planeta! Além, é claro, da tecnologia RAY TRACING.', 'Placadevideo', '5899.90'),
+(null, 'Gigabyte Z390 Aorus', 'z390.jpg', 7, 'Placa mãe Gigabyte Z390 Aorus, se você está procurando uma placa mãe, provavelmente é essa.', 'Placamae', '1690.90'),
+(null, 'Gigabyte X399 Aorus Gaming','x399.jpg', 6, 'Placa mãe Gigabyte X399 Aorus Gaming, uma grande placa mãe para grandes processadores, e é claro que são os Threadrippers.', 'Placamae', '2310.50'),
+(null, 'Intel I9 9900K Skylake', 'I9K.jpg', 9, 'Experimente o que é poder de processamento com a nova linha de processadores I9 da 9ª geração. Diga adeus aos gargalos!', 'Processador', '2860.90'),
+(null, 'AMD Ryzen Threadripper 2950X', 8, 'threadripper.jpg', 'Criado para o trabalho mais pesado possível, pode ter certeza que ele aguenta qualquer coisa.', 'Processador', '3399.90'),
+(null, 'NZXT H500I', 'H500I.jpg', 3, 'Gabinete mid-tower compacto apropriado para ambientes minimalistas, uma beleza de nível de um deus Grego.','Gabinete','739.90'),
+(null, 'Cooler Master COSMOS', 'cosmos.jpg', 1, 'Gabinete full-tower desnecessáriamente grande, porém você irá achar incrívelmente bonito e com certeza terá vontade de comprar.', 'Gabinete', '3160.79'),
+(null, 'Corsair H150I', 'h150i.jpg', 5, 'Watercooler que deixará o resfriamento à nitrogênio líquido no chinelo! Pode levar para casa que não irá se arrepender.', 'Cooler', '1390.45'),
+(null, 'Cooler Master V8', 'V8.jpg', 0, 'Cooler à ar mais potente do planeta terra, bom, o nome já diz tudo, potência de esfriar um motor V8', 'Cooler', '940.90'),
+(null, 'Fita LED 6803', 'LED6803.jpg', 2, 'LED RGB de 5M com controlador e fonte. Cada LED é controlado separadamente', 'Led', '68.90'),
+(null, 'Fita LED 5050 - Branco', 'LED5050.jpg', 5, 'LED branca de 5M com controlador e fonte. Os LEDs são manipulados conjuntamente', 'Led', '39.90'),
+(null, 'Apple iPhone 11', 'iphone11.png', 18, 'O mais novo smarthphone da Apple guiado pelo sistema operacional iOS 13 com um hardware extremamente forte.', 'Smartphones', '5299.90'),
+(null, 'Samsung M30', 'M30.jpg', 17, 'O smartphone com melhor custo benefício do mercado, possuindo uma das mais duráveis baterias e uma tela OLED. É incrível!', 'Smartphones', '1359.90'),
+(null, 'Monitor Alienware AW2518HF', 'alienware.jpg', 2, 'O monitor com maior taxa de atualização no mercado, chegando em até 240hz. Agora você verá até as balas!', 'Periferico', '2499.90'),
+(null, 'Teclado GFallen Falcão-Peregrino', 'Falcao.jpg', 14, 'O melhor teclado finalmente chegou na Kabum Tega, experimente já a maciez e precisão dos Red Switches mais avançados do planeta!', 'Periferico', '443.33'),
+(null, 'HyperX Revolver S', 'HyperX-Cloud-Revolver-S-0.jpg', 15, 'Um dos melhores Headsets já produzidos pela humanidade está a venda na Kabum Tega por um preço mais do que acessível! O que está esperando?', 'Periferico', '590.90'),
+(null, 'Razer Mousepad Firefly', 'Firefly.jpg', 0, 'Mosepad Razer com RGB, sinceramente, não faço a mínima ideia de por quê isso existe...', 'Periferico', '419.90');
---
+++ src/php/Produtos.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="../style/MultiDropdown.css"> 
+    <link rel="stylesheet" href="../style/MeuStyle.css">
+    <link rel="stylesheet" href="../style/mainpage.css">
+    <link rel="icon" href="../img/Kt.png">
+    <title>KabumTega</title>
+</head>
+<body>
+    <?php
+        session_start();
+        require 'config.php';
+        require 'conexao.php';
+        require 'database.php';
+    ?>
+    <nav class="navbar navbar-expand-lg navbar-light" id="navbarSKT">
+            <a class="navbar-brand" href="MainPage.html" style="font-size: 40px;">
+                <img src="../img/kticon_yoA_2.ico"  width="50" height="50" class="d-inline-block-top mr-2" id="logo">KabumTega
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarKT">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarKT" >
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                        <a  class="nav-link dropdown-toggle" id="DropdownComputadores" role="button" data-toggle="dropdown" href="#">Computadores</a>
+                        <ul class="dropdown-menu multi-column columns-2" aria-labelledby="DropdownComputadores" style="background-color:#ff9b21;">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <ul class="multi-column-dropdown">
+                                    <li><a href="Produtos.php?type=Placadevideo">Placa de video</a></li>
+                                    <li><a href="Produtos.php?type=Placamae">Placa Mãe</a></li>
+                                    <li><a href="Produtos.php?type=Processador">Processadores</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-6">
+                                <ul class="multi-column-dropdown">
+                                    <li><a href="Produtos.php?type=Gabinete">Gabinete</a></li>
+                                    <li><a href="Produtos.php?type=Cooler">Cooler</a></li>
+                                    <li><a href="Produtos.php?type=Led">LED</a></li>
+                                </ul>
+                            </div>
+                        </div>                  
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Produtos.php?type=Smartphones">Smartphones</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Produtos.php?type=Periferico">Periféricos</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Login/Register</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="searchContainer">
+        <div class="container">
+            <form class="ml-4 mr-4 my-auto d-inline w-100">
+                <div class="input-group">
+                    <input type="text" class="form-control border-right-0" placeholder="Buscar"> 
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-primary border-left-0" type="button" id="btnSearch">IR</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="barra1"></div>
+    <div class="barra2"></div>
+    
+    <div class="container">
+        <div class="recomendTitle">
+            <a class="recomendados">
+                <?php
+                    $category = '';
+                    switch($_GET['type']){
+                        case 'Placadevideo': echo('Placas de Vídeo'); $category = 'Placadevideo';
+                        break;
+                        case 'Placamae': echo('Placas mãe'); $category = 'Placamae';
+                        break;
+                        case 'Processador': echo('Processadores'); $category = 'Processador';
+                        break;
+                        case 'Gabinete': echo('Gabinetes'); $category = 'Gabinete';
+                        break;
+                        case 'Cooler': echo('Coolers'); $category = 'Cooler';
+                        break;
+                        case 'Led': echo('LEDs'); $category = 'Led';
+                        break;
+                        case 'Periferico': echo('Periféricos'); $category = 'Periferico';
+                        break;
+                        case 'Smartphones': echo('Smartphones'); $category = 'Smartphones';
+                        break;
+                    } 
+                ?>
+            </a>
+        </div>
+        <!-- Bootstrap Cards for good -->
+        <?php $product = DBread('Produto', "WHERE Categoria = '$category'"); $i = 0; while($i < sizeof($product)) : ?>
+        
+        <div class="card-deck">
+            <?php while($i < sizeof($product)) : ?>
+            <div class="barra1" style="margin-top: 10px; margin-bottom: 10px;"></div>
+            <?php if($product[$i]['Quantidade'] > 0) : ?>
+            <div class="card">
+                <img src="../img/<?= $product[$i]['Imagem']; ?>" class="card-img-top" alt="<?= $product[$i]['Titulo']; ?>">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $product[$i]['Titulo']; ?></h5>
+                    <p class="card-text"><?= $product[$i]['Descricao']; ?></p>
+                    <p class="card-text quantidade"><?= $product[$i]['Quantidade'];?></p>
+                    <p class="card-text">Preço: R$<?= $product[$i]['Preco']; $i++;?></p>
+                    <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+            </div>
+            
+            <?php else : ?>
+            <div class="card">
+                <img src="../img/<?= $product[$i]['Imagem']; ?>" class="card-img-top" alt="<?= $product[$i]['Titulo']; ?>">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $product[$i]['Titulo']; ?></h5>
+                    <p class="card-text"><?= $product[$i]['Descricao']; ?></p>
+                    <p class="card-text quantidade"><?= $product[$i]['Quantidade'];?></p>
+                    <p class="card-text">Preço: R$<?= $product[$i]['Preco']; $i++;?></p>
+                    <a href="#" class="btn btn-primary disabled" aria-disabled="true">Fora de Estoque</a>
+                </div>
+            </div>
+            <?php endif;?>
+            <?php if(($i % 3) == 0){break;}?>
+            <?php endwhile; ?>
+        </div>
+        <br>
+        <?php endwhile; ?>
+    </div>
+    <div class="barra1" style="margin-top: 10vh"></div>
+    <div class="espaco"></div>
+    <div class="barra2"></div>
+    <script src="../node_modules/jquery/dist/jquery.js"></script>
+    <script src="../node_modules/popper.js/dist/umd/popper.js"></script>
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.js"></script> 
+</body>
+</html>
