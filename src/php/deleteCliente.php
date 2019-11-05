0440ff1906bbce2ef03b38d77ca6830069646295
GustavoLamb <gustavo.lamb@uol.com.br>
2019-11-04 21:37:21 -0300

    Crud e Sessão prontos

--- src/php/deleteCliente.php
+++ src/php/deleteCliente.php
@@ -2,8 +2,13 @@
     session_start();
     require 'config.php';
     require 'conexao.php';
-    require 'database.php';   
-    DBDelete('cliente',"{$_SESSION['user'][0]['Id_cliente']}");   
-    session_abort();
-    header('Location:MainPage.html');
+    require 'database.php';
+    var_dump($_GET['sair']);
+    if($_GET['sair']){
+        session_destroy();
+    } else {
+        DBDelete('cliente',"{$_SESSION['user'][0]['Id_cliente']}");   
+        session_destroy(); 
+    }
+    header('Location:MainPage.php');
 ?>
